a36e04eb19d7122351a412b9bab4722ac2ae9f26
kevin <taingkn@gmail.com>
2017-12-18 16:22:25 +0100

    serv

--- app/index.php
+++ app/index.php
@@ -4,7 +4,7 @@ require 'conf.inc.php';
 
 try {
     $bdd = new PDO('mysql:host='.DBHOST.';dbname='.DBNAME.';charset=utf8', DBUSER, DBPASSWORD);
-    phpinfo();
+    echo 'OK';
 } catch (Exception $e) {
     die('Erreur : ' . $e->getMessage());
 }
