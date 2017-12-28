abc11efe971c1c16cc39b9b5a8f971360dd91b58
Kevin Taing <taingkn@gmail.com>
2017-12-28 15:51:43 +0100

    test

--- app/github/pull.php
+++ app/github/pull.php
@@ -1,11 +1,8 @@
 <?php
 
-if ( $_POST['payload'] ) {
     $exec = shell_exec( 'git reset --hard HEAD && git pull && cp /var/www/annuel/config/conf.inc.php /var/www/annuel/app/conf.inc.php' );
     echo $exec;
-} else {
-    echo 'NOT OK';
-}
+
 
 ?>
 
