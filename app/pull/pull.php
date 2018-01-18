2397291ca6e4972c599eefeccaa82c46cdb2ee1a
Kevin Taing <taingkn@gmail.com>
2018-01-18 14:55:04 +0100

    test

--- app/pull/pull.php
+++ app/pull/pull.php
@@ -3,6 +3,8 @@
 if ($_POST['payload']) {
 	$exec = shell_exec('cd ../../ && git reset --hard HEAD && git pull git@github.com:taingk/annuel.git master && cp config/conf.inc.php app/');
 	echo $exec;
+	$test = shell_exec("Salut");
+	echo $test;
 	echo 'ok';
 } else {
 	echo 'Not a post request';
