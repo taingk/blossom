46a13672e185c013cf4360f4357e49b596757360
taingk <taingkn@gmail.com>
2018-01-18 11:11:49 +0100

    change path

--- app/github/pull.php
+++ app/github/pull.php
@@ -1,6 +1,6 @@
 <?php
 
-    $exec = shell_exec( 'git reset --hard HEAD && git pull && cp /var/www/annuel/config/conf.inc.php /var/www/annuel/app/conf.inc.php' );
+    $exec = shell_exec( 'git reset --hard HEAD && git pull && cp ../../config/conf.inc.php ../' );
     echo $exec;
 
 
