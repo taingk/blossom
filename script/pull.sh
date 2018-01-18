90421a11730f1dc16349b7b2608710553f3ab757
taingk <taingkn@gmail.com>
2018-01-18 14:37:54 +0100

    ss

--- script/pull.sh
+++ script/pull.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-cd ../ && git reset --hard HEAD && git pull git@github.com:taingk/annuel.git master
-cp ../config/conf.inc.php ../app
+cd ../ && git reset --hard HEAD && git pull git@github.com:taingk/annuel.git master && cp config/conf.inc.php app
