42549b2619c9339232785f237b381dcdaacd6873
taingk <taingkn@gmail.com>
2018-01-18 15:02:19 +0100

    done

--- app/conf.inc.php
+++ app/conf.inc.php
@@ -6,6 +6,3 @@ define('DBHOST', 'blossomofrannuel.mysql.db');
 define('DBNAME', 'blossomofrannuel');
 define('DBPORT', '3306');
 
-define('DS', DIRECTORY_SEPARATOR);
-$scriptName = (dirname($_SERVER["SCRIPT_NAME"]) == "/") ? "" : dirname($_SERVER["SCRIPT_NAME"]);
-define('DIRNAME', $scriptName.DS);
--- app/pull/pull.php
+++ app/pull/pull.php
@@ -1,9 +1,9 @@
 <?php
 
-if ($_POST['payload']) {
-	echo shell_exec('cd ../../ && git reset --hard HEAD && git pull git@github.com:taingk/annuel.git master && cp config/conf.inc.php app/');
-} else {
-	echo 'Not ok';
-}
+//if ($_POST['payload']) {
+	echo shell_exec('git reset --hard HEAD && git pull git@github.com:taingk/annuel.git master && cp ../../config/conf.inc.php ../');
+//} else {
+//	echo 'Not a post request';
+//}
 
 ?>
