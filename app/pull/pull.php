688337ecc0c035f00e35754853438471e03f775d
taingk <taingkn@gmail.com>
2018-01-18 14:53:15 +0100

    test

--- app/pull/pull.php
+++ app/pull/pull.php
@@ -1,8 +1,11 @@
 <?php
 
-
-$exec = shell_exec('cd ../../ && git reset --hard HEAD && git pull git@github.com:taingk/annuel.git master && cp config/conf.inc.php app/');
-echo $exec;
-echo 'ok';
+if ($_POST['payload']) {
+	$exec = shell_exec('cd ../../ && git reset --hard HEAD && git pull git@github.com:taingk/annuel.git master && cp config/conf.inc.php app/');
+	echo $exec;
+	echo 'ok';
+} else {
+	echo 'Not a post request';
+}
 
 ?>
