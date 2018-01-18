702b8201fb7c7510d0c4475e7a5e453d7dc3dc60
taingk <taingkn@gmail.com>
2018-01-18 11:55:26 +0100

    path

--- app/github/pull.php
+++ app/github/pull.php
@@ -1,11 +1,11 @@
 <?php
 
-if ($_POST['payload']) {
-    $exec = shell_exec( 'cd ../../ && git reset --hard HEAD && git pull' );
+//if ($_POST['payload']) {
+    $exec = shell_exec( 'cd ../../ && git reset --hard HEAD && git pull git@github.com:taingk/annuel.git master' );
     shell_exec('cp ../../config/conf.inc.php ../');
     echo $exec;
     echo "ok";
-}
+//}
 
 ?>
 
