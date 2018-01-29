b58cf6b962e60874d7a53ea68d87570571de89b5
taingk <taingkn@gmail.com>
2018-01-29 19:41:16 +0100

    test

--- app/pull/index.php
+++ app/pull/index.php
@@ -3,7 +3,7 @@
 chdir(dirname(__FILE__));
 
 if ($_POST['payload']) {
-	echo shell_exec('git reset --hard HEAD && git pull git@github.com:taingk/annuel.git master && cp ../../config/conf.inc.php ../');
+	echo exec('git reset --hard HEAD && git pull git@github.com:taingk/annuel.git master && cp ../../config/conf.inc.php ../');
 	echo 'Post ok';
 } else {
 	echo 'Not a post request';
