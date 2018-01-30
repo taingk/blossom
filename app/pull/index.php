a3c04ab4eec52ca4b2e2a18b3cb4c95be463e57c
kevin <taingkn@gmail.com>
2018-01-30 15:04:19 +0100

    pull

--- app/pull/index.php
+++ app/pull/index.php
@@ -3,7 +3,7 @@
 chdir(dirname(__FILE__));
 
 if ($_POST['payload']) {
-	echo exec('/usr/bin/git reset --hard HEAD && /usr/bin/git pull  2>&1 && cp ../../config/conf.inc.php ../');
+	echo shell_exec('/usr/bin/git pull  2>&1 && cp ../../config/conf.inc.php ../');
 	//echo shell_exec("/usr/bin/git pull 2>&1");
 	//echo shell_exec("/usr/bin/git pull 2&gt;&amp;1");
 	//echo shell_exec("/usr/bin/git status");
