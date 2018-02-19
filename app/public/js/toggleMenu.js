5aea983844dad807ef7bf5b5d426daec5999f66f
Kevin taing <taingkn@gmail.com>
2018-02-19 15:53:27 +0100

    admin bar not done

---
+++ app/public/js/toggleMenu.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    if (document.getElementById('menu').className == 'ouvert') {
+        document.getElementById('menu').className = '';
+    } else {
+        document.getElementById('menu').className = 'ouvert';
+    }
+}
