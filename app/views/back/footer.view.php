4bbadb6b600469dfbd10421bd9766f8352c17d08
Enzo Huynh <enzo_huynh@hotmail.com>
2018-02-27 16:59:42 +0100

    backFooter done

--- app/views/back/footer.view.php
+++ app/views/back/footer.view.php
@@ -1,5 +1,4 @@
 <nav class="row bgFooter">
-    <div class="col-lg-12">
-        <span class="copyright">© 2018 Blossom. Powered by ESGI. All Rights Reserved.</span>
-    </div>
+    <span class="col-lg-8 copyright">© 2018 Blossom. Powered by ESGI. All Rights Reserved.</span>
+    <span class="col-lg-4 langue">Langue Français</span>
 </nav>
