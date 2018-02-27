36770e5512549078f0d5539feea0f343cfa9cecf
lavan350 <lprepanantha@myges.fr>
2018-02-27 10:44:00 +0100

    create adminBar2

--- app/views/templates/back2.tpl.php
+++ app/views/templates/back2.tpl.php
@@ -102,18 +102,8 @@
 
 <body>
     <header>
-        <div class="col-xs-12">
-            <div class="row">
-                <div class="col-xs-2"></div>
-                <div class="col-xs-10" id="menuStyle">
-                    <span>
-                        <!-- <a href=""> -->
-                        <img src="../../../public/img/icons8-menu-100.png" alt="" height="30px" width="30px" id="hideBtn">
-                        <!-- </a> -->
-                    </span>
-                </div>            
-            </div>
-        </div>        
+        <!-- Barre rouge pour display ou hide menu lateral -->
+        <?php include("../back/modules/adminBar2.php"); ?>
     </header>
 
 
