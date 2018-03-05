28d8866056b400a9d580dc2e6d79be78c37545a7
Kevin Taing <taingkn@gmail.com>
2018-03-05 11:17:13 +0100

    icon turned into class

--- app/public/js/iconManager.js
+++ app/public/js/iconManager.js
@@ -1,25 +1,33 @@
-let aClass = [];
+class iconManager {
+    constructor() {
+        this.aClass = [];
+        this.getClasses();
+        this.setIcon();
+    }
 
-function getClasses() {
-    let aClass = [];
+    getClasses() {
+        const self = this;
 
-    $('body *:not(script)').each(function () {
-        sClass = $(this).attr('class') ? $(this).attr('class').split(' ') : [];
+        $('body *:not(script)').each(function () {
+            let sClass = $(this).attr('class') ? $(this).attr('class').split(' ') : [];
 
-        sClass.forEach(sValue => {
-            if (aClass.indexOf(sValue) < 0 && sValue.substring(0, 5) === "icon_") {
-                aClass.push(sValue)
-            }
+            sClass.forEach(sValue => {
+                if (self.aClass.indexOf(sValue) < 0 && sValue.substring(0, 5) === "icon_") {
+                    self.aClass.push(sValue);
+                }
+            });
         });
-    });
+    }
 
-    return aClass;
+    setIcon() {
+        this.aClass.forEach(sClass => {
+            $(`.${sClass}`).css({
+                "background-image": `url(/public/icon/${sClass.substr(5)}.svg)`,
+                "height": "40px",
+                "width": "40px"
+            });
+        });
+    }
 }
 
-getClasses().forEach(sClass => {
-    $(`.${sClass}`).css({
-        "background-image": `url(/public/icon/${sClass.substr(5)}.svg)`,
-        "height": "40px",
-        "width": "40px"
-    });
-});
+new iconManager();
