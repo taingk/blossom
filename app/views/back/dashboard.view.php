0d8f4ddf20ee57200f1a406abf9e7ace6a042436
Kevin taing <taingkn@gmail.com>
2018-03-10 00:53:34 +0100

    responsive view

--- app/views/back/dashboard.view.php
+++ app/views/back/dashboard.view.php
@@ -1,27 +1,27 @@
-<article class="col-xs-12 col-sm-12 col-md-6 col-lg-6 gutters">
+<article class="col-xs-12 col-lg-6 gutters">
     <h1 class="rose">Statistique utilisateurs</h1>
-    <canvas id="canvas" class="chartjs-render-monitor margin-auto" style="height: 200px; width: 100%"></canvas>
+    <canvas id="canvas1" class="margin-auto chartjs" ></canvas>
 </article>
 
-<article class="col-xs-12 col-sm-12 col-md-6 col-lg-6 gutters">
+<article class="col-xs-12 col-lg-6 gutters">
     <h1 class="rose">Statistique produits</h1>
-    <canvas id="chart-area" class="chartjs-render-monitor margin-auto" style="height: 200px; width: 100%"></canvas>
+    <canvas id="canvas2" class="margin-auto chartjs" ></canvas>
 </article>
 
-<article class="col-xs-12 col-sm-12 col-md-6 col-lg-6 gutters">
+<article class="col-xs-12 col-lg-6 gutters">
     <h1 class="rose">Statistique ventes</h1>
-    <canvas id="canvas2" class="chartjs-render-monitor margin-auto" style="height: 200px; width: 100%"></canvas>
+    <canvas id="canvas3" class="margin-auto chartjs" ></canvas>
 </article>
 
-<article class="col-xs-12 col-sm-12 col-md-6 col-lg-6 gutters">
+<article class="col-xs-12 col-lg-6 gutters">
     <h1 class="rose">Statistique visibilités</h1>
-    <canvas id="chart-area2" class="chartjs-render-monitor margin-auto" style="height: 200px; width: 100%"></canvas>
+    <canvas id="canvas4" class="margin-auto chartjs" ></canvas>
 </article>
 
 <!-- Chartjs -->
 <script src="/public/js/lib/Chart.bundle.min.js"></script>
 <script src="/public/js/lib/Chart.min.js"></script>
-<script src="http://www.chartjs.org/samples/latest/utils.js"></script>
+<script src="/public/js/chart.utils.js"></script>
 <script src="/public/js/chartjs1.js"></script>
 <script src="/public/js/chartjs2.js"></script>
 <script src="/public/js/chartjs3.js"></script>
