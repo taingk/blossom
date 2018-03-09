e005e724b33724ef0982fe12bd1e68ef75338355
Kevin taing <taingkn@gmail.com>
2018-03-09 22:27:51 +0100

    view presque

--- app/views/back/dashboard.view.php
+++ app/views/back/dashboard.view.php
@@ -1,21 +1,21 @@
-<article class="col-sm-6">
+<article class="col-xs-12 col-sm-12 col-md-6 col-lg-6 gutters">
     <h1 class="rose">Statistique utilisateurs</h1>
-    <canvas id="canvas" class="chartjs-render-monitor"></canvas>
+    <canvas id="canvas" class="chartjs-render-monitor margin-auto" style="height: 200px; width: 100%"></canvas>
 </article>
 
-<article class="col-sm-6">
+<article class="col-xs-12 col-sm-12 col-md-6 col-lg-6 gutters">
     <h1 class="rose">Statistique produits</h1>
-    <canvas id="chart-area" class="chartjs-render-monitor"></canvas>
+    <canvas id="chart-area" class="chartjs-render-monitor margin-auto" style="height: 200px; width: 100%"></canvas>
 </article>
 
-<article class="col-sm-6">
+<article class="col-xs-12 col-sm-12 col-md-6 col-lg-6 gutters">
     <h1 class="rose">Statistique ventes</h1>
-    <canvas id="canvas2" class="chartjs-render-monitor"></canvas>
+    <canvas id="canvas2" class="chartjs-render-monitor margin-auto" style="height: 200px; width: 100%"></canvas>
 </article>
 
-<article class="col-sm-6">
+<article class="col-xs-12 col-sm-12 col-md-6 col-lg-6 gutters">
     <h1 class="rose">Statistique visibilités</h1>
-    <canvas id="chart-area2" class="chartjs-render-monitor"></canvas>
+    <canvas id="chart-area2" class="chartjs-render-monitor margin-auto" style="height: 200px; width: 100%"></canvas>
 </article>
 
 <!-- Chartjs -->
--- app/views/front/footer.view.php
+++ app/views/front/footer.view.php
@@ -1,1 +1,5 @@
-<p><p>
+<nav class="row bgFooter">
+    <div class="col-xs-12 margin-auto">
+        <p>© 2018 Blossom. Powered by ESGI. All Rights Reserved.</p>
+    </div>
+</nav>
