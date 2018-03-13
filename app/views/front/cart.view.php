310103328b929a7dcc5b78cdc225063bdde3db23
Kevin taing <taingkn@gmail.com>
2018-03-13 16:41:51 +0100

    panier responsive

--- app/views/front/cart.view.php
+++ app/views/front/cart.view.php
@@ -1,5 +1,5 @@
-<section class="row container bg-color">
-    <h1 class="col-xs-12">Article de votre panier</h1>
+<section class="row container mainView box-shadow">
+    <h1 class="col-xxs-12">Votre panier</h1>
     
     <hr>
 
@@ -19,17 +19,17 @@
                     </figure>
                 </td>
                 <td>
-                    <div class="captionProduct">
-                        <p><strong>iPhone 8</strong> - Blanc - 250 Go</p>
-                        <p class="grey">Garantie 2 ans</p>
-                        <p class="grey">Référence : DHL06B2</p>
+                    <div class="captionProduct responsive-center">
+                        <p><strong>iPhone 8</strong></p>
+                        <p class="grey"><strong>Blanc - 250 Go</strong></p>
+                        <p class="grey">N° réf: DHL06B2</p>
                     </div>
                 </td>
                 <td>
-                    <p class="left">759,00€</p>
+                    <p class="left grey">759,00€</p>
                 </td>
                 <td>
-                    <p class="left">x 1</p>
+                    <p class="left grey">x 1</p>
                 </td>
                 <td>
                     <div class="text-left">
@@ -45,17 +45,16 @@
                     </figure>
                 </td>
                 <td>
-                    <div class="captionProduct">
+                    <div class="captionProduct responsive-center">
                         <p><strong>AirPod</strong></p>
-                        <p class="grey">Garantie 1 an</p>
-                        <p class="grey">Référence : BMW24R8</p>
+                        <p class="grey">N° réf: BMW24R8</p>
                     </div>
                 </td>
                 <td>
-                    <p class="left">250,00€</p>
+                    <p class="left grey">250,00€</p>
                 </td>
                 <td>
-                    <p class="left">x 2</p>
+                    <p class="left grey">x 2</p>
                 </td>
                 <td>
                     <div class="text-left">
@@ -69,20 +68,22 @@
 
     <hr>
 
-    <section class="col-xs-7"></section>
-    <section class="col-xs-5 row line-height">
-        <article class="col-xs-11 col-md-8 col-lg-6 text-right"><p>Sous-total du panier :</p></article>
-        <article class="col-xs-12 col-md-4 col-lg-6"><p>1259,00€</p></article>
-        <article class="col-xs-11 col-md-8 col-lg-6 text-right"><p>Livraison :</p></article>
-        <article class="col-xs-12 col-md-4 col-lg-6"><p>00,00€</p></article>
-        <article class="col-xs-11 col-md-8 col-lg-6 text-right"><p><strong>Total :</p></article>
-        <article class="col-xs-12 col-md-4 col-lg-6"><p>1259,00€</strong></p></article>
+    <section class="col-xxs-12 row line-height">
+        <article class="col-xs-4 col-md-6"></article>
+        <article class="col-xxs-7 col-xs-6 col-md-4 text-right"><p class="grey">Sous-total du panier :</p></article>
+        <article class="col-xxs-5 col-xs-2"><p class="grey">1259,00€</p></article>
+        <article class="col-xs-4 col-md-6"></article>
+        <article class="col-xxs-7 col-xs-6 col-md-4 text-right"><p class="grey">Livraison :</p></article>
+        <article class="col-xxs-5 col-xs-2"><p class="grey">00,00€</p></article>
+        <article class="col-xs-4 col-md-6"></article>
+        <article class="col-xxs-7 col-xs-6 col-md-4 text-right"><p><strong>Total :</p></article>
+        <article class="col-xxs-5 col-xs-2"><p>1259,00€</strong></p></article>
     </section>
 
-    <div class="col-xs-5 col-sm-5 col-md-7 col-lg-7"></div>
-    <div class="col-xs-7 col-sm-7 col-md-5 col-lg-5 btn">
+    <div class="col-xs-5 col-md-7"></div>
+    <div class="col-xxs-12 col-sm-7 col-md-5 btn">
         <form action="/front/billing" method="post">
-            <input class="btnPayer" type="submit" value="Payer" onclick="alert('Merci de votre commande')"/>
+            <input class="btnPayer right" type="submit" value="Payer" onclick="alert('Merci de votre commande')"/>
         </div>
     </form>
 </section>
