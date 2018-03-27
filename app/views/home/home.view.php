aefdd20564c630990f1973146eca63ca3971e58f
lavan350 <lprepanantha@myges.fr>
2018-03-27 16:06:03 +0200

    commit

--- app/views/home/home.view.php
+++ app/views/home/home.view.php
@@ -1,33 +1,33 @@
 <section id="example1" class="homeSlogan">
-    <h2 style="text-align: center; color: black">Six raisons d'utiliser <span style="color: #fc4348">Blossom</span></h2>
-    <hr style="width: 30%">
-    <div class="sixReasons col-lg-6" style="margin: 0 auto;">
+    <h2 class="black">Six raisons d'utiliser <span class="rose">Blossom</span></h2>
+    <hr id="greatLine">
+    <div class="sixReasons col-lg-6 margin-auto">
         <div class="row">
-            <div class="col-lg-4 boxFigure">
+            <div class="col-lg-4 col-sm-8 boxFigure">
                 <div class="content">
                     <figure class="homeFigure">
                         <img src="../../public/img/brushIcon.png" class="homeFeaturesImg" alt=".." title="...">
-                        <figcaption><h2 style="color:black">De nombreux design</h2></figcaption>
+                        <figcaption><h2 class="black">De nombreux design</h2></figcaption>
                     </figure>
                     <hr>
                     <p style="color: black;font-size: 14px;text-align: justify;">Referente penetrali timebantur in disceret conscii timebantur Marcio susurrasset vitae in interdum Amphiarao disceret etiam disceret susurrasset arcanorum susurrasset postridie Marcio referente secreto velut vitae secreto nullo secreto praesente vatibus quondam postridie conscii ut vitae conscii postridie aurem siquid paterfamilias velut siquid inclitis vatibus aut parietes timebantur aurem parietes ideoque.</p>
                 </div>
             </div>
-            <div class="col-lg-4 boxFigure">
+            <div class="col-lg-4 col-sm-8 boxFigure">
                 <div class="content">
                     <figure class="homeFigure">
                         <img src="../../public/img/securityIcon.png" class="homeFeaturesImg" alt=".." title="...">
-                        <figcaption><h2 style="color:black">De la sécurité</h2></figcaption>
+                        <figcaption><h2 class="black">De la sécurité</h2></figcaption>
                     </figure>
                     <hr>
                     <p style="color: black;font-size: 14px;text-align: justify;">Referente penetrali timebantur in disceret conscii timebantur Marcio susurrasset vitae in interdum Amphiarao disceret etiam disceret susurrasset arcanorum susurrasset postridie Marcio referente secreto velut vitae secreto nullo secreto praesente vatibus quondam postridie conscii ut vitae conscii postridie aurem siquid paterfamilias velut siquid inclitis vatibus aut parietes timebantur aurem parietes ideoque.</p>
                 </div>
             </div>
-            <div class="col-lg-4 boxFigure">
+            <div class="col-lg-4 col-sm-8 boxFigure">
                 <div class="content">
                     <figure class="homeFigure">
                         <img src="../../public/img/perfIcon.png" class="homeFeaturesImg" alt=".." title="...">
-                        <figcaption><h2 style="color:black">Des performances</h2></figcaption>
+                        <figcaption><h2 class="black">Des performances</h2></figcaption>
                     </figure>
                     <hr>
                     <p style="color: black;font-size: 14px;text-align: justify;">Referente penetrali timebantur in disceret conscii timebantur Marcio susurrasset vitae in interdum Amphiarao disceret etiam disceret susurrasset arcanorum susurrasset postridie Marcio referente secreto velut vitae secreto nullo secreto praesente vatibus quondam postridie conscii ut vitae conscii postridie aurem siquid paterfamilias velut siquid inclitis vatibus aut parietes timebantur aurem parietes ideoque.</p>
@@ -35,31 +35,31 @@
             </div>                
         </div>
         <div class="row">
-            <div class="col-lg-4 boxFigure">
+            <div class="col-lg-4 col-sm-8 boxFigure">
                 <div class="content">
                     <figure class="homeFigure">
                         <img src="../../public/img/thumbUpIcon.png" class="homeFeaturesImg" alt=".." title="...">
-                        <figcaption><h2 style="color:black">Facile d’utilisation</h2></figcaption>
+                        <figcaption><h2 class="black">Facile d’utilisation</h2></figcaption>
                     </figure>                
                     <hr>
                     <p style="color: black;font-size: 14px;text-align: justify;">Referente penetrali timebantur in disceret conscii timebantur Marcio susurrasset vitae in interdum Amphiarao disceret etiam disceret susurrasset arcanorum susurrasset postridie Marcio referente secreto velut vitae secreto nullo secreto praesente vatibus quondam postridie conscii ut vitae conscii postridie aurem siquid paterfamilias velut siquid inclitis vatibus aut parietes timebantur aurem parietes ideoque.</p>
                 </div>
             </div>
-            <div class="col-lg-4 boxFigure">
+            <div class="col-lg-4 col-sm-8 boxFigure">
                 <div class="content">
                     <figure class="homeFigure">
                         <img src="../../public/img/mouseIcon.png" class="homeFeaturesImg" alt=".." title="...">
-                        <figcaption><h2 style="color:black">Prêt en un clic</h2></figcaption>
+                        <figcaption><h2 class="black">Prêt en un clic</h2></figcaption>
                     </figure>
                     <hr>
                     <p style="color: black;font-size: 14px;text-align: justify;">Referente penetrali timebantur in disceret conscii timebantur Marcio susurrasset vitae in interdum Amphiarao disceret etiam disceret susurrasset arcanorum susurrasset postridie Marcio referente secreto velut vitae secreto nullo secreto praesente vatibus quondam postridie conscii ut vitae conscii postridie aurem siquid paterfamilias velut siquid inclitis vatibus aut parietes timebantur aurem parietes ideoque.</p>
                 </div>
             </div>
-            <div class="col-lg-4 boxFigure">
+            <div class="col-lg-4 col-sm-8 boxFigure">
                 <div class="content">
                     <figure class="homeFigure homeMobile">
                         <img src="../../public/img/phoneIcon.png" class="homeFeaturesImg" alt=".." title="...">
-                        <figcaption><h2 style="color:black">Responsive mobile</h2></figcaption>
+                        <figcaption><h2 class="black">Responsive mobile</h2></figcaption>
                     </figure>            
                     <hr>
                     <p style="color: black;font-size: 14px;text-align: justify;">Referente penetrali timebantur in disceret conscii timebantur Marcio susurrasset vitae in interdum Amphiarao disceret etiam disceret susurrasset arcanorum susurrasset postridie Marcio referente secreto velut vitae secreto nullo secreto praesente vatibus quondam postridie conscii ut vitae conscii postridie aurem siquid paterfamilias velut siquid inclitis vatibus aut parietes timebantur aurem parietes ideoque.</p>
@@ -70,15 +70,21 @@
 </section>
 
 <section id="example2" style="position: relative">
-    <div class="container" style="position: absolute;bottom: 0;left: 0;right: 0;">
-        <div class="col-sm-8" style="margin:0 auto">
-            <div class="windowsFrame">
-                <div class="windowsControl"></div>
-                <div class="windowsControl"></div>
-                <div class="windowsControl"></div>
+    <div style="position: absolute;bottom: 0;left: 0;right: 0;">
+        <div class="mainContent" style="display: table; margin: 0 auto;">
+            <div class="col-sm-5" style="float:left">
+                <div class="windows">
+                    <div class="windowsContent">
+                        <img src="../../public/img/1.jpg" alt="" style="height: 300px;width: auto;padding:2px 0">
+                    </div>
+                </div>
             </div>
-            <div class="windowsContent">
-                <img src="http://image.noelshack.com/fichiers/2018/12/6/1521919337-deepin-capture-ecran-zone-de-selection-20180324202154.png" alt="">
+            <div class="col-sm-5" style="float:left">
+                <div class="windows">
+                    <div class="windowsContent">
+                        <img src="../../public/img/3.jpg" alt="" style="height: 300px;width: auto;padding:2px 5px">
+                    </div>
+                </div>
             </div>
         </div>
     </div>
