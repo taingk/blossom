698ab5dc7205aead6c4f2677936e98ba09821491
Enzo Huynh <enzo_huynh@hotmail.com>
2018-03-30 14:48:47 +0200

    frontHome done

--- app/views/front/homePage.view.php
+++ app/views/front/homePage.view.php
@@ -7,9 +7,18 @@
 <section class="row">
     <div class="col-xss-12 col-md-6">
         <img class="frontFirstCol gutters" src="../../public/img/samsung.jpg">
+        <input class="savoir-plus2" type="submit" value="En savoir plus">
     </div>
     <div class="col-xss-12 col-md-6">
         <img class="frontSecondCol gutters" src="../../public/img/lg.jpg">
+        <input class="savoir-plus3" type="submit" value="En savoir plus">
+    </div>
+    <div class="col-xss-12">
+        <img class="frontThirdCol" src="../../public/img/galaxy2.jpg">
+        <h2>Galaxy S9 | S9+<h2>
+        <p class="galaxy2Desc">Samsung réinvente la photo avec les Galaxy S9 et S9+. <br>
+Prenez des clichés éclatants, dans la pénombre comme en plein jour. <br>
+Suspendez le temps pour révéler vos plus beaux instants.</p>
     </div>
 
 </section>
