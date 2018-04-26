b7a79dac1b69c66bd3c87cb21b693a9bf4a87795
lavan350 <lprepanantha@myges.fr>
2018-04-26 09:47:48 +0200

    prototype all items

--- app/controllers/front/CartController.class.php
+++ app/controllers/front/CartController.class.php
@@ -6,7 +6,7 @@ class CartController {
     * View listing des articles 
     */ 
     public function indexAction( $aParams ) {
-        $oView = new View("cart", "front");
+        $oView = new View("products", "front");
         $oProduct = new Products();
         $aResult = $oProduct->select();
         $oView->assign('products', $aResult);
