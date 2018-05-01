c14584dc266bda57cc729d42ef947b0923d24e4c
unknown <taing_k@etna-alternance.net>
2018-05-01 16:22:12 +0200

    get age refactor

--- app/core/Helper.class.php
+++ app/core/Helper.class.php
@@ -3,16 +3,7 @@
 class Helper {
 
     static function getAge( $sBirthDay ) {
-        // Création d'un objet dateTime a partir de la date de naissance
-        $oDateTime = new DateTime($sBirthDay);
-        // Que l'on va comparer avec la date d'aujourd'hui
-        $oToday = new DateTime();
-        // Calcul de la différence entre les deux dates
-        $iDifference = $oToday->diff($oDateTime);
-        // On récupère la différence en année
-        $iAge = $iDifference->y;
-        
-        return $iAge;
+        return floor( ( time() - strtotime( $sBirthDay ) ) / 31556926 );
     }
 
     static function getSexe( $iSexe ) {
