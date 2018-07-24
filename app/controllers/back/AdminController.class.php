e79373fe8c345df92e7cca1acfacea693c966f4c
Amandine Ripoll <amandine-ripoll@outlook.fr>
2018-07-24 09:56:19 +0200

    FunctionSitemap

--- app/controllers/back/AdminController.class.php
+++ app/controllers/back/AdminController.class.php
@@ -60,5 +60,13 @@ class AdminController {
         $_SESSION = [];
         header('Location: /back');
     }
+
+    public function siteMapAction()
+        {
+
+            echo $_SERVER['SERVER_NAME'];
+            header('Content-Type: text/xml; charset=UTF-8');
+            echo '<?xml version="1.0" encoding="UTF-8"?>'."\n";
+        }
     
 }
